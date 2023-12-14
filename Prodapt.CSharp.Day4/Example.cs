915739d5b6eb2cee04c89a694e18bb742b3c5187
ramyasri.as <ramyasri.as@CLAP1181>
2023-12-14 11:17:00 +0530

    Add project files.

---
+++ Prodapt.CSharp.Day4/Example.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Prodapt.CSharp.Library;
+
+namespace Prodapt.CSharp.Day4
+{
+    internal class Example
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example1");
+            Login obj = new Login();
+            var result = obj.Validation("admin", "admin@123");
+            Console.WriteLine(result ? "Valid User" : "Invalid User");
+                }
+    }
+}
---
+++ Prodapt.CSharp.Day4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Prodapt.CSharp.Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Now;
+            Console.WriteLine($"Today:{today}");
+            Console.WriteLine($"DateTime:{today.ToShortDateString()}");
+            Console.WriteLine($"DateOnly:{DateTime.Now.ToShortDateString()}");
+            Console.WriteLine($"TimeOnly:{today.ToShortTimeString}");
+            Console.WriteLine($"TimeOnly:{DateTime.Now.ToShortTimeString()}");
+            Console.WriteLine($"LongDate:{today.ToLongDateString()}");
+            Console.WriteLine($"LongTImeOnly:{today.ToLongTimeString()}");
+        }
+    }
+}
